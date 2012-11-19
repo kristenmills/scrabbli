b2e9e1bbe0a654528618fb4db76b01ba48ef89c3
kristenmills <kristen@kristen-mills.com>
2012-11-19 18:27:34 -0500

    Optimized getting the next move. Now only takes 33 seconds. fixed #8

--- lib/scrabbli.rb
+++ lib/scrabbli.rb
@@ -9,11 +9,11 @@ require File.join(File.dirname(__FILE__), 'scrabbli', 'board_square')
 require File.join(File.dirname(__FILE__), 'scrabbli', 'player')
 require File.join(File.dirname(__FILE__), 'scrabbli', 'game')
 
-hand = ["A", "R", "G", "I", "S", "T", "*"]
+hand =  ["A", "R", "Y", "G", "I", "*", "T", "*"] 
 player =  Scrabble::Player.new hand
 
-puts player.make_next_move
 Benchmark.bm do |x|
 	x.report {player.make_next_move}
 end
+
 # puts player.traverse "UDBKFB"
--- lib/scrabbli/player.rb
+++ lib/scrabbli/player.rb
@@ -1,13 +1,16 @@
+require 'set'
 module Scrabble
 
 	class Player
 		attr_accessor :score, :tiles
 
+		#Create a new Scrabble Player
 		def initialize  tiles=nil, score=0
 			@score = score
 			@tiles = tiles
 		end
 
+		#create the tile hash
 		def create_tile_hash
 			tile_hash = Hash.new 0
 			@tiles.each do |x|
@@ -16,9 +19,10 @@ module Scrabble
 			tile_hash
 		end
 
-		def wildcard hands
-			other_array = hands
-			while (other_array[0].include? '*')
+		#Replace all of the wildcars with actual characters and then filter out non words
+		def filter k, v
+			other_array = v
+			k.times do
 				array = Array.new
 				other_array.each do |x|
 					'A'.upto('Z') do |y|
@@ -27,12 +31,29 @@ module Scrabble
 				end
 				other_array = array
 			end
-			other_array
+			other_array.reject{|val| !Scrabble::DICTIONARY.has_key?(val)}
 		end
 
+		#return the characters in the word before the first asterisk
+		def prefix word
+			word.split('*')[0]
+		end
+
+		#get all of the real words
+		def get_words perms
+			set = Set.new
+			perms.each do |k,v|
+				set.merge(getWords(k,v))
+			end
+			set
+		end
+
+		#returns the highest scoring word
 		def best words
 			tile_hash = create_tile_hash
 			best_word = ['',0]
+			
+			best_word
 			words.each do |word|
 				split_word = word.split(//)
 				score = 0
@@ -50,20 +71,31 @@ module Scrabble
 			best_word
 		end
 
+		#makes the next move
 		def make_next_move 
-			hands = Array.new 
+			prefix_hash = Hash.new
 			@tiles.count.downto(2) do |x|
-				hands.concat(@tiles.permutation(x).to_a.map{|x| x = x.join})
+				ti = @tiles.permutation(x)
+				ti.each do |y|
+					c = prefix(y.join)
+					prefix_hash[c] ||= Set.new
+					prefix_hash[c] << y.join
+				end
 			end
-			hands = wildcard(hands)
-
-			words = Array.new
-			hands.each do |x|
-  			words << x if Scrabble::DICTIONARY.has_key?(x)
+			words =Hash.new 
+			count = 0
+			prefix_hash.each do |p, v|
+				if(Scrabble::DICTIONARY.children(p).count != 0)
+					count +=1
+					v.each do |y|
+						c = y.count "*"
+						words[c] ||= Set.new
+						words[c] << y
+					end
+				end
 			end
-			tile_hash = create_tile_hash
-			best words
+			words = blanks(words)
+ 			best(words)[0]
 		end
 	end
 end
-
