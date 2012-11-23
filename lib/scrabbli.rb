b7d59cb6354f903989b741a615c44b88725d36b3
kristenmills <kristen@kristen-mills.com>
2012-11-23 14:30:32 -0500

    removed that test directory

--- lib/scrabbli.rb
+++ lib/scrabbli.rb
@@ -14,26 +14,3 @@ require File.join(File.dirname(__FILE__), 'scrabbli', 'player')
 require File.join(File.dirname(__FILE__), 'scrabbli', 'game')
 require File.join(File.dirname(__FILE__), 'scrabbli', 'cli_helpers')
 require File.join(File.dirname(__FILE__), 'scrabbli', 'cli') 
-# hand =  "FORE*TRY" 
-# player =  Scrabble::Player.new("Kristen", hand.split(//))
-# # puts "Answer: #{Scrabble::DICTIONARY.has_key? "CHEDDAR"}"
-# # Benchmark.bm do |x|
-# # 	x.report {Scrabble::DICTIONARY.has_key? "CHEDDAR"}
-# # end
-# # puts Scrabble::DICTIONARY.has_key? "APPLE"
-
-# #puts player.get_possible_words.to_a.count
-#  puts player.get_next_moves.to_a
-# Benchmark.bm do |x|
-# 	#x.report {player.get_possible_words }
-# 	 x.report {player.get_next_moves }
-# end
-
-# # # puts player.traverse "UDBKFB"
-
-# # begin
-# # 	Scrabble::CLI.run
-# # rescue Interrupt
-# # 	puts 
-# # 	Scrabble::Helpers::exit_message
-# # end
